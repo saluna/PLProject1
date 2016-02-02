2a9f3fe796d90ae29cf77102051a39ab9471f2d9
kjiwani <kjiwani786@gmail.com>
2016-02-01 23:18:31 -0600

    Deleted Random "head" and other notations, Removed unwanted code and deleted the main method in customer.java because that would fix the issue of typecasting. however the program is not running for me. I'm not sure.

--- src/kothari/customer/CustomerDB.java
+++ src/kothari/customer/CustomerDB.java
@@ -43,26 +43,3 @@ public class CustomerDB {
         return customer;
     }
 }
-
-//        if(custNum == 1001){
-//            return "1001\nBarbara White\n" +
-//                    "3400 Richmond Parkway #3423\n" +
-//                    "Bristol, CT 06010";
-//        }
-//        else if (custNum == 1002){
-//            return "1002\nKarl Vang\n327 Franklin Street\nEdina, MN 55435\n";
-//        }
-//        else if (custNum == 1003){
-//            return "1003\nRonda Chavan\n" +
-//                    "518 Commanche Dr.\n" +
-//                    "Greensboro, NC 27410";
-//        }
-//        else
-//            return "There is no customer number " + custNum + " in our records.";
-//
-//    }
-//
-//	private static void else if(boolean b) {
-//		// TODO Auto-generated method stub
-//
-//	}
